526fceb35ea7c27f76f8c1f84c4d481d5114aae7
Astolfo-Official <74457956+Astolfo-Official@users.noreply.github.com>
2026-03-21 19:10:52 +0000

    Deploying to gh-pages from @ Astolfo-Official/Astolfo-Official.github.io@6cd5a83bfcd63c98e539a4e089f65e4d14a95064 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -483,8 +483,8 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=8WzTBusAAAAJ&hl", "_blank");
         },
       },{
-        id: 'social-github',
-        title: 'GitHub',
+        id: 'social-custom_social',
+        title: 'Custom_social',
         section: 'Socials',
         handler: () => {
           window.open("https://github.com/Astolfo-Official", "_blank");

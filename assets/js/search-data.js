916ab9652241660ee36e02d7936723ec7b2bc747
Astolfo-Official <74457956+Astolfo-Official@users.noreply.github.com>
2026-05-07 14:26:24 +0000

    Deploying to gh-pages from @ Astolfo-Official/Astolfo-Official.github.io@6a5224297664dde6a2f4edd5172c548dd5acb0ea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/notes/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "post-complex-shifts-in-the-gaussian-integral",
         
           title: "Complex Shifts in the Gaussian Integral",

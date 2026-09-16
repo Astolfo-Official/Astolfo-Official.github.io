4072b9103ab31584a0c6e1d11fb6ab44dadcacd0
Astolfo-Official <86186998@qq.com>
2026-09-16 12:26:53 -0400

    Update publications and citation workflow

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,4 +3,7 @@ module.exports = {
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
+  // Pie-chart topics are assigned at runtime via element.dataset.topic.
+  // Preserve their attribute selectors and the color variables they define.
+  dynamicAttributes: ["data-topic"],
 };
